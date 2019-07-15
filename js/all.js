9f2d750060a06f143a9cc6e5915e3010f5a4d9ac
bell881122 <51412772+bell881122@users.noreply.github.com>
2019-07-15 23:58:07 +0800

    201907152358 update

--- js/all.js
+++ js/all.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+    $('.no-href').click(function (e) {
+        $(this).find('a');
+        e.preventDefault();
+    });
+
+    
     $('.showmenu').on('click', function (e) {
         e.preventDefault();
         $('body').toggleClass('menu-show');
@@ -8,5 +15,13 @@ $(document).ready(function () {
         e.preventDefault();
         $('.login-box, .signin-box').toggleClass('active');
     })
+
+    $('.fa-heart').click(function (e) {
+        // e.preventDefault();
+        $(this).toggleClass('heart-active');
+        console.log(1111111);
+    });
+
+
 
 });
